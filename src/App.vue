b4bace0f857fa2adee0e702169e415aa16a343b9
Michael <m.tunik@isoit.ru>
2021-06-06 20:40:33 +0300

    Add title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <h1>Hello Vue</h1>
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
